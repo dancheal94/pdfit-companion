45b184d06addf137e511a041eb85b2e63021c837
Dan Cheal <dancheal@MacBook-Air-3.local>
2026-08-04 11:22:42 +0200

    Simplify: minimal working versions

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,54 +1,18 @@
 using System.Windows;
-using PDFitCompanion.Services;
 using Serilog;
 
 namespace PDFitCompanion
 {
     public partial class App : Application
     {
-        private AuthService? _authService;
-        private SpoolMonitor? _spoolMonitor;
-        private NotificationService? _notificationService;
-
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-
             Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .WriteTo.File(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-                    "PDFit", "Logs", "pdfit-companion-.txt"),
-                    rollingInterval: RollingInterval.Day)
+                .MinimumLevel.Information()
+                .WriteTo.File("pdfit-companion.log")
                 .CreateLogger();
-
-            Log.Information("PDFit Companion starting...");
-
-            try
-            {
-                _notificationService = new NotificationService();
-                _authService = new AuthService();
-                _spoolMonitor = new SpoolMonitor(_authService, _notificationService);
-
-                _spoolMonitor.Start();
-                Log.Information("Spool monitor started");
-
-                new MainWindow().Show();
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Failed to start PDFit Companion");
-                MessageBox.Show($"Failed to start PDFit Companion: {ex.Message}", "Error",
-                    MessageBoxButton.OK, MessageBoxImage.Error);
-                Shutdown(1);
-            }
-        }
-
-        protected override void OnExit(ExitEventArgs e)
-        {
-            Log.Information("PDFit Companion shutting down");
-            _spoolMonitor?.Dispose();
-            Log.CloseAndFlush();
-            base.OnExit(e);
+            Log.Information("PDFit Companion started");
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,30 +1,13 @@
 using System.Windows;
-using PDFitCompanion.Services;
 
 namespace PDFitCompanion
 {
     public partial class MainWindow : Window
     {
-        private NotifyIconService? _trayIcon;
-
         public MainWindow()
         {
             InitializeComponent();
-            Loaded += MainWindow_Loaded;
-        }
-
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            _trayIcon = new NotifyIconService();
-            _trayIcon.ShowMessage("PDFit Companion started", "Ready to print to PDF");
-            Hide();
-        }
-
-        protected override void OnStateChanged(EventArgs e)
-        {
-            if (WindowState == WindowState.Minimized)
-                Hide();
-            base.OnStateChanged(e);
+            this.Hide();
         }
     }
 }
